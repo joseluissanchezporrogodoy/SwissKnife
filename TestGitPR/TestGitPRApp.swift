f24d858b2459766c5a64dcfe25e128ed621b0427
José Luis Sánchez-Porro Godoy <joseluis.sanchezporro.godoy@gmail.com>
2024-10-03 17:42:26 +0200

    Initial Commit

---
+++ TestGitPR/TestGitPRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitPRApp.swift
+//  TestGitPR
+//
+//  Created by JLSANCHEZP on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGitPRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
